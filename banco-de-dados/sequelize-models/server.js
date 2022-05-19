cf5385ec08dab0a59ab095c4e7fafee9db552a53
Isaque Voss Klassmann <isaque.iv@gmail.com>
2022-05-18 21:32:57 -0300

    aula 18-05

--- banco-de-dados/sequelize-models/server.js
+++ banco-de-dados/sequelize-models/server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const jwt = require('jsonwebtoken')
+const cors = require('cors')
 const { sequelize } = require("./db");
 const { Aluno } = require('./models/aluno_model');
 const { Professor } = require('./models/professor_model');
@@ -7,6 +9,60 @@ const { User } = require('./models/user_model');
 const { Turma } = require('./models/turma_model')
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
+
+const APP_KEY = 'APP_KEY';
+const APP_REFRESH_KEY = 'APP_REFRESH_KEY'
+
+app.post('/users', async (req, res) => {
+    const user = new User();
+    user.email = req.body.email;
+    user.password = req.body.password;
+    await user.save();
+    res.send(user);
+})
+app.post('/login', async (req, res) => {
+    const user = await User.findOne({ where: { email: req.body.email } });
+    if (!user) {
+        return res.status(400).send('User not found');
+    }
+    if (user.password !== req.body.password) {
+        return res.status(400).send('Invalid password');
+    }
+    return res.json({
+        accessToken: jwt.sign({ userId: user.id }, APP_KEY, { expiresIn: 60 }),
+        refreshToken: jwt.sign({ userId: user.id }, APP_REFRESH_KEY, { expiresIn: 60 * 60 * 24 * 7 })
+    });
+});
+
+app.post('/valid-token', (req, res) => {
+    try {
+        jwt.verify(req.body.accessToken, APP_KEY);
+        res.json({ message: 'JWT is valid' });
+    } catch (error) {
+        res.json({ message: 'JWT is invalid' });
+    }    
+})
+
+app.post('/token/refresh', (req,res) => {
+    const payload = jwt.verify(req.body.refreshToken, APP_REFRESH_KEY);
+    if (!payload) {
+        return res.status(400).send('Invalid refresh token');
+    }
+    return res.json({
+        accessToken: jwt.sign({ userId: payload.userId }, APP_KEY, { expiresIn: 60 }),
+        refreshToken: jwt.sign({ userId: payload.userId }, APP_REFRESH_KEY, { expiresIn: 60 * 60 * 24 * 7 })
+    });
+})
+
+app.use((req, res, next) => {
+    try {
+        jwt.verify(req.headers.authorization, APP_KEY);
+        next();
+    } catch (error) {
+        res.json({ message: 'Usuário não está autorizado' }).sendStatus(401);
+    }
+})
 
 app.get('/alunos', async (req, res) => {
     const alunos = await Aluno.findAll();
@@ -57,6 +113,8 @@ app.delete('/alunos/:id', async (req, res) => {
     await aluno.destroy();
     return res.json({ message: 'Aluno removido com sucesso' });
 });
+
+
 
 app.listen(3000, async () => {
     await sequelize.sync();
